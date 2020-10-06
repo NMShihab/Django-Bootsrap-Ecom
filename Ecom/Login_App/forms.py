22c0a545f62b56ce86723291f13b0abf54464da5
NMShihab <nmshihabislam@gmail.com>
2020-10-06 13:17:14 +0600

    custom signup and profile

--- Ecom/Login_App/forms.py
+++ Ecom/Login_App/forms.py
@@ -1,9 +1,27 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
+from Login_App.models import User
 
 class SignUpForm(UserCreationForm):
+    email = forms.EmailField(required=True,widget=forms.TextInput(
+    attrs={
+    'placeholder':'Email',
+    'class':'form-group'
+    }))
+
+    password1 = forms.CharField(required=True,widget=forms.PasswordInput(
+    attrs={
+    'placeholder':'Password',
+    'class':'form-group'
+    }))
+    password2 = forms.CharField(required=True,widget=forms.PasswordInput(
+    attrs={
+    'placeholder':'Confirm Password',
+    'class':'form-group'
+    }))
+
 
     class Meta:
     	model = User
-    	fields = ["username", "password1", "password2"]
+    	fields = ["email", "password1", "password2"]
